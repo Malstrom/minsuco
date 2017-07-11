44bbf50a1b28fb1b4d7e1699935597df3e959261
Igor Mironenko <igormir87@gmail.com>
2017-07-11 15:13:40 +0200

    first commit

---
+++ app/assets/javascripts/charts.js
@@ -0,0 +1,27 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://coffeescript.org/
+
+//--- Sparklines
+//= require sparkline/index
+//--- Flot
+//= require flot/jquery.flot
+//= require flot.tooltip/js/jquery.flot.tooltip.min
+//= require flot/jquery.flot.resize
+//= require flot/jquery.flot.pie
+//= require flot/jquery.flot.time
+//= require flot/jquery.flot.categories
+//= require flot-spline/js/jquery.flot.spline.min
+//= require chart.js/dist/Chart
+//--- Chartist
+//= require chartist/dist/chartist
+//--- Morris
+//= require raphael/raphael
+//= require morris.js/morris
+//--- Rickshaw
+//= require d3/d3.min
+//= require rickshaw/rickshaw
+// --- Knob
+//= require jquery-knob/js/jquery.knob.js
+// --- EasyPie
+//= require jquery.easy-pie-chart/dist/jquery.easypiechart.js
---
+++ app/models/user.rb
@@ -0,0 +1,65 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  devise :omniauthable, :omniauth_providers => [:facebook]
+
+  has_many :authorizations
+  has_one :subscription, ->(sub) { where.not(stripe_id: nil) }, class_name: Payola::Subscription, foreign_key: :owner_idx
+
+  enum role: [:user, :admin, :silver, :gold, :platinum]
+  after_initialize :set_default_role, :if => :new_record?
+  after_initialize :set_default_plan, :if => :new_record?
+  # after_create :sign_up_for_mailing_list
+
+  belongs_to :plan
+  validates_associated :plan
+
+  def set_default_role
+    self.role ||= :user
+  end
+
+  def set_default_plan
+    self.plan ||= Plan.last
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  # def self.from_omniauth(auth)
+  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #     user.email = auth.info.email
+  #     user.password = Devise.friendly_token[0,20]
+  #     user.name = auth.info.name   # assuming the user model has a name
+  #     user.image = auth.info.image # assuming the user model has an image
+  #   end
+  # end
+
+  def self.from_omniauth(auth)
+    authorization = Authorization.where(:provider => auth.provider, :uid => auth.uid.to_s).first_or_initialize
+    authorization.token = auth.credentials.token
+    if authorization.user.blank?
+      user = User.where('email = ?', auth["info"]["email"]).first
+      if user.blank?
+        user = User.new
+        user.password = Devise.friendly_token[0,10]
+        user.email = auth.info.email
+        user.name = auth.info.name   # assuming the user model has a name
+        user.image = auth.info.image # assuming the user model has an image
+
+        user.save
+      end
+      authorization.user_id = user.id
+    end
+    authorization.save
+    authorization.user
+  end
+
+end

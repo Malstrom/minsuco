7aa55fc8d9e7212b52ce09736d11ec7f9c74fa27
Igor Mironenko <igormir87@gmail.com>
2017-07-16 17:27:25 +0200

    remove type from attendees table

--- db/migrate/20170713161221_create_attendees.rb
+++ db/migrate/20170713161221_create_attendees.rb
@@ -3,7 +3,6 @@ class CreateAttendees < ActiveRecord::Migration[5.1]
     create_table :attendees do |t|
       t.belongs_to :attendee, foreign_key: { to_table: :users }, index: true
       t.belongs_to :race, index: true
-      t.string     :type
       t.timestamps
     end
   end

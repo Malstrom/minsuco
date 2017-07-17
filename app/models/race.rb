f0ee1351ececdd362d6d0e753980ee15bbba4a47
Igor Mironenko <igormir87@gmail.com>
2017-07-17 16:12:17 +0200

    remove validations end_date_is_after_start_date

--- app/models/race.rb
+++ app/models/race.rb
@@ -6,18 +6,11 @@ class Race < ApplicationRecord
   has_many :attendees
   has_many :attedees, :class_name => "User", :through => "attendees", :foreign_key => "attendee_id"
 
-  validates_presence_of :start_date, :end_date
 
-  validate :end_date_is_after_start_date
+  validates_presence_of :starts_at, :ends_at
+
 
   private
 
-  def end_date_is_after_start_date
-    return if starts_at.blank? || ends_at.blank?
-
-    if starts_at < ends_at
-      errors.add(:end_date, "cannot be before the start date")
-    end
-  end
 
 end

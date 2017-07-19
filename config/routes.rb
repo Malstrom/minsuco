a778dab2c6d3753a976df4ffcd0e8161e23538cc
Igor Mironenko <igormir87@gmail.com>
2017-07-19 18:00:35 +0200

    add to get actions start and pause to races resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :races
+
   mount Payola::Engine => '/payola', as: :payola
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :subscriptions
+  resources :races do
+    get :start
+    get :pause
+  end
 
   resources :onboarding, :path => '/on-boarding' do
     collection do

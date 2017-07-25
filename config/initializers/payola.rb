dbc062e072f6240a8dcffd8f69d73f24d7608b5e
Igor Mironenko <igormir87@gmail.com>
2017-07-25 05:59:05 +0200

    add event handling when payola success payments

--- config/initializers/payola.rb
+++ config/initializers/payola.rb
@@ -1,11 +1,41 @@
-Payola.configure do |config|
-  config.secret_key = Rails.application.secrets.stripe_api_key
-  config.publishable_key = Rails.application.secrets.stripe_publishable_key
+Payola.configure do |payola|
+  payola.secret_key = Rails.application.secrets.stripe_api_key
+  payola.publishable_key = Rails.application.secrets.stripe_publishable_key
 
-  Payola.subscribe 'customer.subscription.deleted' do |event|
-    sale = Sale.find_by(stripe_id: event.data.object.id)
-    user = User.find_by(email: sale.email)
-    UserMailer.expire_email(user).deliver
-    user.destroy
+  payola.default_currency = 'eur'
+
+  # before execute charge.
+  # payola.charge_verifier = lambda do |sale, custom_data|
+  #   if sale.product_type == 'Race'
+  #     race = Race.find(sale.product_id)
+  #     race.update_attribute(:status, 'started')
+  #     race.update_attribute(:kind, 'pay_for_publish')
+  #   end
+  # end
+
+
+  #
+  payola.subscribe 'payola.sale.finished' do |sale|
+    race = Race.find(sale.product_id)
+    race.update_attribute(:status, 'started')
+    race.update_attribute(:kind, 'pay_for_publish')
   end
+
+  #
+  # payola.subscribe 'payola.race.sale.failed' do |sale|
+  #   race = Race.find(sale.product_id)
+  #   flash[:success] = "Pagamento fallito, prego riprova. #{sale.error}"
+  #   raise 'Abbiamo avuto un problema nel pagamento'
+  # end
+  #
+
+
+
+
+  # Payola.subscribe 'customer.subscription.deleted' do |event|
+  #   sale = Sale.find_by(stripe_id: event.data.object.id)
+  #   user = User.find_by(email: sale.email)
+  #   UserMailer.expire_email(user).deliver
+  #   user.destroy
+  # end
 end

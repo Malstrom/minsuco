8b999fab34a39d18d233f99216e181ccb851902b
Igor Mironenko <igormir87@gmail.com>
2017-08-04 17:32:14 +0200

    set dafault value

--- db/schema.rb
+++ db/schema.rb
@@ -202,8 +202,8 @@ ActiveRecord::Schema.define(version: 20170804152637) do
 
   create_table "rewards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
-    t.integer "public_races"
-    t.integer "join_private"
+    t.integer "public_races", default: 0
+    t.integer "join_private", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_rewards_on_user_id"

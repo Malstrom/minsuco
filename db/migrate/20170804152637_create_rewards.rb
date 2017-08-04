2ca5d4e2caff3fe328990d77e144a0f199c2bc15
Igor Mironenko <igormir87@gmail.com>
2017-08-04 17:34:28 +0200

    set default values to 3

--- db/migrate/20170804152637_create_rewards.rb
+++ db/migrate/20170804152637_create_rewards.rb
@@ -2,8 +2,8 @@ class CreateRewards < ActiveRecord::Migration[5.1]
   def change
     create_table :rewards do |t|
       t.belongs_to :user, foreign_key: true, index: true
-      t.integer :public_races, :default => 0
-      t.integer :join_private, :default => 0
+      t.integer :public_races, :default => 3
+      t.integer :join_private, :default => 3
 
       t.timestamps
     end

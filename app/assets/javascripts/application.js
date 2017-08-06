492b5ac0305d0151f8839902a38c5df9c436de33
Igor Mironenko <igormir87@gmail.com>
2017-08-06 17:30:57 +0200

    heroku deploying

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,6 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-
-
-//= require d3/d3
-//= require underscore/underscore
-
 //= require payola
 
 //--- Angle
@@ -14,5 +9,3 @@
 //= require jquery_nested_form
 
 ////= require cable
-//= require jquery
-//= require jquery_ujs

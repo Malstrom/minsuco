18511664e853ff0ed01044fbc5c527bd6cd03122
Igor Mironenko <igormir87@gmail.com>
2017-08-06 16:25:51 +0200

    Add first ability for testing

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,24 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+      user ||= User.new # guest user (not logged in)
+      if user.admin?
+        can :manage, :all
+      elsif user.pro_attendee?
+        can :read, Race
+        can :new, Race
+        can :create, Race
+        can :attendees, Race
+        can :user_races, Race
+        can :publish_new, Race
+        can :publish_check, Race, Race do |race|
+          if race.kind == 'pay_for_publish'
+            true if current_user.pro_creator? or race.owner.have_reward?('public_publish')
+          else
+            true
+          end
+        end
+      end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.

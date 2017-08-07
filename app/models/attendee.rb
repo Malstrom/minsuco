717c8b0e231a1ceb45939c0b7851c4e633a5292c
Igor Mironenko <igormir87@gmail.com>
2017-08-07 19:11:18 +0200

    unify validation to one call, add joinable method, move create event to down and replace Event.new with event.create, remove self.

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -3,45 +3,42 @@ class Attendee < ApplicationRecord
   belongs_to :attendee, :class_name => 'User'
   belongs_to :race
 
-  validate :self_join
-  validate :max_attendee
-  validate :unique_join
+  validate :max_attendee, :unique_join, :joinable, :self_join
 
   after_create_commit { create_event }
 
   private
 
-  def create_event
-
-    event = Event.new
-
-    event.thing_type = 'Attendee'
-    event.thing_id = id
-    event.owner = attendee
-    event.recipient = race.owner
-    event.notifiable = true
-    event.read = false
-    event.message = "join in race"
-
-    event.save
+  # check if attendee has permission to join the race (subscription, kind of race, free join token, ecc...)
+  def joinable
+    unless attendee.has_plan_for_join? or attendee.has_reward?('pay_for_join')
+      errors.add(:joinable, "Non è possibile partecipare alla gara, controlla se il tuo abbonamento lo prevede")
+    end
   end
 
   def self_join
-    if self.race.owner_id == self.attendee_id
+    if race.owner_id == attendee_id
       errors.add(:self_join, "A user cannot be an attendee of its race")
     end
   end
 
   def max_attendee
-    if self.race.attendees.count >= self.race.max_attendees
+    if race.attendees.count >= race.max_attendees
       errors.add(:max_attendee, "Race reach max attedee")
     end
   end
 
   def unique_join
-    if Attendee.exists?(attendee: self.attendee, race: self.race)
+    if Attendee.exists?(attendee: attendee, race: race)
       errors.add(:unique_join, "User must join only one time")
     end
   end
 
+  # create event every type create attendee
+  def create_event
+    Event.create(thing_type: 'Attendee', thing_id:id, message:'join in race',
+                 owner: attendee, recipient: race.owner,
+                 notifiable: true, read: false
+    )
+  end
 end

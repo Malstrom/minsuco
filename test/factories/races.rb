65cc2e1e2e97b36c6a4660dbca249e0ef3eb8663
Igor Mironenko <igormir87@gmail.com>
2017-08-13 17:31:03 +0200

    bug fix see current_user image and not @user

--- test/factories/races.rb
+++ test/factories/races.rb
@@ -8,8 +8,8 @@ FactoryGirl.define do
     recipients "all"
     race_value "100000"
     category_id 5
-    starts_at Time.now.strftime("%m/%d/%Y")
-    ends_at Time.now.strftime("%m/%d/%Y")
+    starts_at Time.now.strftime("%d/%m/%Y")
+    ends_at((Time.now + 100.hours).strftime("%d/%m/%Y") )
     status "started"
     kind "pay_for_join"
     permalink "#{Time.now.to_i}-TestRace"

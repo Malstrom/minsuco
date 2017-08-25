5d2b442db5dfbed56616ac326ea31c6dace1a651
Igor Mironenko <igormir87@gmail.com>
2017-08-25 14:59:59 +0200

    improve code
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,17 +2,16 @@ class EventsController < ApplicationController
   def readed
     event = Event.find(params[:id])
 
-    event.update_attribute(:read, true)
+    event.update(read: true)
 
     if event.thing_type == 'Attendee' and Attendee.exists?(id: event.thing_id)
-      attendee = Attendee.find(event.thing_id)
-      race = Race.find(attendee.race.id)
 
-      redirect_to race_path(race)
+      redirect_to race_path(Attendee.find(event.thing_id).race)
+
     elsif event.thing_type == 'Race' and Race.exists?(id: event.thing_id)
-      race = Race.find(event.thing_id)
 
-      redirect_to race_path(race)
+      redirect_to race_path(Race.find(event.thing_id))
+
     else
       flash[:warning] = "Notifica scaduta!"
       redirect_to root_path

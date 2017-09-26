074bfa8b8641a69f9ba6e9db59e460ff3fc27593
Igor Mironenko <igormir87@gmail.com>
2017-09-26 20:01:07 +0200

    remove sparkline
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,6 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
-//--- Sparklines
-//= require sparkline/index
+
 //--- Flot
 //= require flot/jquery.flot
 //= require flot.tooltip/js/jquery.flot.tooltip.min

9d1c112ac196666db4f5a79f76e32da2e232787b
Igor Mironenko <igormir87@gmail.com>
2017-10-07 17:38:26 +0200

    remove first year scope and add find_by_duration, best, worst
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- app/models/commission.rb
+++ app/models/commission.rb
@@ -1,5 +1,9 @@
 class Commission < ApplicationRecord
   belongs_to :race
 
-  scope :first_year,   -> { order('starts ASC').first }
+  # find nearest commission by duration
+  scope :find_by_duration,  ->(duration) { where("duration < ?", duration).order("ABS( duration - #{duration} )").max}
+
+  scope :best,   -> { order('value DESC') }
+  scope :worst,  -> { order('value DESC') }
 end

f5e153fda20a8111a8b82719176e2a9806142bfe
Igor Mironenko <igormir87@gmail.com>
2017-10-10 14:06:43 +0200

    remove dist jquery from base.js
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -1,7 +1,5 @@
 // This is a manifest file that'll be compiled into base.js, which will include all the files
 // listed below.
-//--- jQuery
-//= require jquery/dist/jquery
 //--- Bootstrap
 //= require bootstrap/dist/js/bootstrap
 //--- Storage API

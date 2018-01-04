afda2f14dc7dbdab6a106ec9791f06405c542764
Igor Mironenko <igormir87@gmail.com>
2018-01-04 13:14:23 +0300

    comment modernizr cause is offline
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -1,7 +1,7 @@
 // This is a manifest file that'll be compiled into base.js, which will include all the files
 // listed below.
 //--- Modernizr
-//= require modernizr/modernizr.custom
+
 //--- jQuery
 //= require jquery/dist/jquery
 //--- Bootstrap

3e61283cd1465d1734acf5ad1058b38e25686a46
Igor Mironenko <igormir87@gmail.com>
2018-01-22 17:07:48 +0100

    add kind of change name and valu of plans
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- app/services/create_plan_service.rb
+++ app/services/create_plan_service.rb
@@ -1,25 +1,25 @@
 class CreatePlanService
   def call
-    p1 = Plan.where(name: 'Enterprise').first_or_initialize do |p|
-      p.amount = 2900
+    p1 = Plan.where(name: 'basic').first_or_initialize do |p|
+      p.amount = 0
       p.interval = 'month'
-      p.stripe_id = 'enterprise'
+      p.stripe_id = 'basic'
     end
     p1.save!
-    p2 = Plan.where(name: 'Premium').first_or_initialize do |p|
-      p.amount = 1900
+    p2 = Plan.where(name: 'light').first_or_initialize do |p|
+      p.amount = 400
       p.interval = 'month'
-      p.stripe_id = 'premium'
+      p.stripe_id = 'light'
     end
     p2.save!(:validate => false)
     p3 = Plan.where(name: 'Pro-creator').first_or_initialize do |p|
-      p.amount = 900
+      p.amount = 2900
       p.interval = 'month'
       p.stripe_id = 'pro_creator'
     end
     p3.save!(:validate => false)
     p4 = Plan.where(name: 'Pro-attendee').first_or_initialize do |p|
-      p.amount = 0
+      p.amount = 900
       p.interval = 'month'
       p.stripe_id = 'pro_attendee'
     end

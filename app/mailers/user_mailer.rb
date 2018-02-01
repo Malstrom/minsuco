00e040ebe1061ff4db70892616c3e27bc68f97ba
Igor Mironenko <igormir87@gmail.com>
2018-02-01 12:07:20 +0300

    mailer for user
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,25 +1,24 @@
 class UserMailer < ApplicationMailer
-
   def welcome(user)
     email_params(user)
-    mail(to: user.email, subject: t('email.invite.in_my_race.subject'))
+    mail(to: user.email, subject: t('email.user.welcome'))
   end
 
   # pass json for transactional email
   def email_params(user)
     headers 'X-SMTPAPI' => {
-        'to' => [user.email],
-        'sub' => {
-            '%nome_cognome%' => [user.name]
-        },
-        'filters' => {
-            'templates' => {
-                'settings' => {
-                    'enable' => 1,
-                    'template_id' => '052314f3-79c1-456f-b9b9-98c4358c17e7'
-                }
-            }
+      'to' => [user.email],
+      'sub' => {
+        '%nome_cognome%' => [user.name]
+      },
+      'filters' => {
+        'templates' => {
+          'settings' => {
+            'enable' => 1,
+            'template_id' => '052314f3-79c1-456f-b9b9-98c4358c17e7'
+          }
         }
+      }
     }.to_json
   end
 end

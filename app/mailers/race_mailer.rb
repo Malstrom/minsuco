21f933bfbde1ab8dc985aa873c98bc67130661f3
Igor Mironenko <igormir87@gmail.com>
2018-02-01 12:07:04 +0300

    mailer for race
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- app/mailers/race_mailer.rb
+++ app/mailers/race_mailer.rb
@@ -1,2 +1,25 @@
 class RaceMailer < ApplicationMailer
+  def owner_emails(race, template_id)
+    email_params(race, template_id)
+    mail(to: race.owner.email)
+  end
+
+  # pass json for transactional email
+  def email_params(race, template_id)
+    headers 'X-SMTPAPI' => {
+      'to' => [race.owner.email],
+      'sub' => {
+        '%nome_cognome%' => [race.owner.name],
+        '%link%' => [race_url(race)]
+      },
+      'filters' => {
+        'templates' => {
+          'settings' => {
+            'enable' => 1,
+            'template_id' => template_id
+          }
+        }
+      }
+    }.to_json
+  end
 end

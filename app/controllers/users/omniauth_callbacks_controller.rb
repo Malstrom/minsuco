910a0715c1f13491203ec5dc8e314fa1e0ef16f6
Igor Mironenko <igormir87@gmail.com>
2018-04-12 17:24:15 +0300

    test data for acce from iarena
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -26,16 +26,24 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def iarena
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_i_arena(request.env['omniauth.auth'])
+    @user = User.new
+    @user.password = Devise.friendly_token[0, 10]
+    @user.email = 'test@gdd.it'
+    @user.name = 'iarena_user'   # assuming the user model has a name
+    # user.image = open(auth.info.image)
+    @user.save
 
-    if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    #@user = User.from_i_arena(request.env['omniauth.auth'])
+
+    #if @user.persisted?
+     # flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Iarena'
       sign_in_and_redirect @user, event: :authentication
-    else
-      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-    end
+    #else
+     # session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
+      #redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    #end
   end
 
   def failure

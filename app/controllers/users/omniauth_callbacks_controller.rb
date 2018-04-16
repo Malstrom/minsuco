13712f60ff3725f72109dc1b22c6b2580e5f8287
Igor Mironenko <igormir87@gmail.com>
2018-04-16 15:39:59 +0300

    test
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -26,11 +26,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def iarena
+    raise 'entrato nel methodo di iarena'
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_i_arena(request.env['omniauth.auth'])
 
     if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Iarena'
       sign_in_and_redirect @user, event: :authentication
     else
       session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores

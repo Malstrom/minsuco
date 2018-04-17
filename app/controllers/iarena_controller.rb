57f35959c3f472f13f60c0a9e813f4258b28a7f8
Igor Mironenko <igormir87@gmail.com>
2018-04-17 12:54:31 +0300

    bug fix sign_in
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- app/controllers/iarena_controller.rb
+++ app/controllers/iarena_controller.rb
@@ -7,11 +7,19 @@ class IarenaController < ApplicationController
   def sign_up
     # create ruby object from json
     auth = JSON.parse(params.to_json, object_class: OpenStruct)
+
+    #token verifying
+    unless auth.credentials.token == Rails.application.secrets.iarena_token
+      redirect_to new_user_registration_url, alert: "Richiesta ricevuta da fonte non autorizzata"
+    end
+
+    #save_user
     @user = User.from_i_arena(auth)
 
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Iarena'
-      sign_in_and_redirect @user, event: :authentication
+      sign_in :user, @user
+      # redirect_to root_path
     else
       session['devise.iarena_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")

4aad1579eef7b7ffe14eae7622fd86fb059de860
Igor Mironenko <igormir87@gmail.com>
2018-04-18 15:47:43 +0300

    test raise
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- app/controllers/iarena_controller.rb
+++ app/controllers/iarena_controller.rb
@@ -17,11 +17,9 @@ class IarenaController < ApplicationController
 
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Iarena'
-      sign_in(@user, scope: :user)
-      raise 'igor funziona'
+      sign_in(@user)
       redirect_to root_path
     else
-      raise 'igor non funziona'
       session['devise.iarena_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end

0a921494b8ac3b0fa51fb64c39d3a4915e60862d
Igor Mironenko <igormir87@gmail.com>
2018-04-19 11:34:36 +0300

    exclude iarena sign_up and sign_in from authrorization
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 
   before_action :set_paper_trail_whodunnit
-  before_action :authenticate_user!, :except => [:public_url, :sign_up, :authorize]
+  before_action :authenticate_user!, :except => [:public_url, :iarena_sign_up, :authorize, :iarena_sign_in]
   before_action :load_notifications, :set_intent
   before_action :set_current_locale
 

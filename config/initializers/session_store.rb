b68bba04dc63f25eefb1a95355f14c545468953a
Igor Mironenko <igormir87@gmail.com>
2018-04-19 13:39:33 +0300

    try flow
    Signed-off-by: Igor Mironenko <igormir87@gmail.com>

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 # Rails.application.config.session_store :cookie_store, key: '_minsuco_session'
 
-Rails.application.config.session_store :active_record_store, :key => '_minsuco_session', domain: :all
+Rails.application.config.session_store :active_record_store, :key => '_minsuco_session'
